01ccadfd91028dd17fad1cf8cb40a33d80857a70
moraalejandra93 <moraalejandra93@gmail.com>
2022-10-16 10:32:36 +1100

    Sending new wip file

---
+++ JavaScript/TodoListProject/todo.js
@@ -0,0 +1,24 @@
+
+let input = prompt("What would you like to do?");
+const todos = ["Collect Chicen eggs", "Clean Litter Box"];
+while (input !== 'quit' && input !== "q"){
+    if (input === "list") {
+        console.log("*************")
+        for (let i = 0; i < todos.length; i++){
+            console.log(`${i}: ${todos}`)
+        }
+        console.log("*************")
+
+    } else if (input === 'new'){
+        const newTodo = prompt("Ok, what else do you want do to:");
+        todos.push(newTodo);
+        console.log(`${newTodo} added to the list`);
+    }
+    else if (input === 'delete'){
+        const index = prompt("Enter an index to delete");
+        console.log(index);
+        
+    }
+    input = prompt("What would you like to do?")
+}
+console.log("You quite the app");
